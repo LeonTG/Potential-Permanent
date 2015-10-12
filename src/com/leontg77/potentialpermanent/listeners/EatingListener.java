b24a7192441a6558568c97189df522314aaff35d
LeonTG77 <leontg77@gmail.com>
2015-10-12 22:22:23 +0200

    Made the code work more efficient

--- src/com/leontg77/potentialpermanent/listeners/EatingListener.java
+++ src/com/leontg77/potentialpermanent/listeners/EatingListener.java
@@ -19,6 +19,8 @@ import com.leontg77.potentialpermanent.Utils;
  * @author LeonTG77
  */
 public class EatingListener implements Listener {
+	private static final float SATURATION_TO_ADD = 9.6f;
+	private static final int FOOD_TO_ADD = 4;
 	
 	@EventHandler
 	public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
@@ -28,13 +30,15 @@ public class EatingListener implements Listener {
 		if (item == null) {
 			return;
 		}
+
+		if (item.getType() == Material.MILK_BUCKET) {
+			player.sendMessage(ChatColor.RED + "You cannot drink milk in PotentialPermanent.");
+			event.setItem(new ItemStack (Material.AIR));
+			event.setCancelled(true);
+			return;
+		}
 		
 		if (item.getType() != Material.GOLDEN_APPLE) {
-			if (item.getType() == Material.MILK_BUCKET) {
-				player.sendMessage(ChatColor.RED + "You cannot drink milk in PotentialPermanent.");
-				event.setItem(new ItemStack (Material.AIR));
-				event.setCancelled(true);
-			}
 			return;
 		}
 		
@@ -42,21 +46,17 @@ public class EatingListener implements Listener {
 		
         player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 1));
         player.getWorld().playSound(player.getLocation(), Sound.BURP, 1, 1);
-        player.setSaturation(player.getSaturation() + 9.6f);
-        player.setFoodLevel(player.getFoodLevel() + 4);
+        player.setSaturation(player.getSaturation() + SATURATION_TO_ADD);
+        player.setFoodLevel(player.getFoodLevel() + FOOD_TO_ADD);
 		event.setCancelled(true);
         
-		if (player.getItemInHand().getAmount() == 1) {
-			player.setItemInHand(new ItemStack (Material.AIR));
-		} else {
-			ItemStack itemInHand = player.getItemInHand();
-			itemInHand.setAmount(itemInHand.getAmount() - 1);
-			player.setItemInHand(itemInHand);
-		}
+		player.getInventory().removeItem(new ItemStack (Material.GOLDEN_APPLE, 1));
 		
 		if (absHearts != 0) {
-			player.setMaxHealth(absHearts >= 4 ? player.getMaxHealth() + 4 : player.getMaxHealth() + absHearts);
-			Utils.setAbsHearts(player, absHearts >= 4 ? absHearts - 4 : 0);
+			float toTake = Math.min(4, absHearts);
+			
+			player.setMaxHealth(player.getMaxHealth() + toTake);
+			Utils.setAbsHearts(player, absHearts - toTake);
 		}
 	}
 }
